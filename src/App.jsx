bf89d06e0b74c5b8961f786d1becce8e8eef8510
Rohit Kumar Bind <rohitbind9@gmail.com>
2022-08-06 10:40:25 +0530

    App changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
 import React from 'react'
 import Calc from './Calc'
-import Footer from './Footer'
+//import Footer from './Footer'
 const App = () => {
     return (
     <>
   <Calc/>
-  <Footer/>
     </>
     
   )
